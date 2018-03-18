e42e005cb49c1b8ca2ffdc791502e9fade8a1598
Swav Kulinski <swav.kulinski@gmail.com>
2018-03-18 08:19:28 +0000

    9th

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:gdg_glasgow_devfest/repository.dart';
 
 void main() => runApp(new MyApp());
 
@@ -8,10 +10,51 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-       primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue,
       ),
-      home: new Text('Hello GDG Glasgow!'),
+      home: new MyScaffols(),
     );
   }
 }
 
+class MyScaffols extends StatefulWidget {
+
+
+  @override
+  MyScaffolsState createState() {
+    return new MyScaffolsState();
+  }
+}
+
+class MyScaffolsState extends State<MyScaffols> {
+
+
+  List<User> _users;
+
+  @override initState() {
+    super.initState();
+    _users = new List();
+    _loadUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) => new Scaffold(
+      appBar: new CupertinoNavigationBar(
+        middle: new Text('Hello GDG Glasgow!'),
+      ),
+        body: new ListView.builder(
+          itemCount: _users.length,
+          itemBuilder: (context,index) => new ListTile(
+            leading: new Text('${_users[index].id}'),
+            title: new Text('${_users[index].name}'),
+            subtitle: new Text('${_users[index].email}'),
+            trailing: new Icon(Icons.chevron_right),
+          ),
+        )
+      );
+
+  void _loadUsers() async {
+    var result = await loadUsersRemote();
+    setState(() => _users = result);
+  } 
+}
